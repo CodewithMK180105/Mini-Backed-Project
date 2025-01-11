9bfa9c4364643bef25303bacadeadd95cd150408
CodewithMK180105 <manishkumar180105@gmail.com>
2025-01-11 12:11:31 +0530

    Implemented like and Unlike feature

--- app.js
+++ app.js
@@ -20,9 +20,20 @@ app.get('/login', (req,res)=>{
 })
 
 app.get('/profile', isLoggedIn, async (req,res)=>{
-    let user=await userModel.findOne({email: req.user.email}).populate("posts");;
-    console.log(user);
+    let user=await userModel.findOne({email: req.user.email}).populate("posts");
+    // console.log(user);
     res.render("profile", {user});
+})
+app.get('/like/:postId', isLoggedIn, async (req,res)=>{
+    let post=await postModel.findOne({_id: req.params.postId}).populate("user");
+
+    if(post.likes.indexOf(req.user.userid)===-1){
+        post.likes.push(req.user.userid);
+    } else {
+        post.likes.splice(post.likes.indexOf(req.user.userid),1);
+    }
+    await post.save();
+    res.redirect("/profile");
 })
 
 app.post('/post', isLoggedIn, async (req,res)=>{
